259e8bba55403657260052129b9f3a955d197d98
hugH5203 <1936489510@qq.com>
2021-09-19 21:32:28 +0800

    也可以提交到码云上，但是要自己选择URL

--- src/main/java/in2021winter/annotation/config/SpringConfiguation.java
+++ src/main/java/in2021winter/annotation/config/SpringConfiguation.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author HuangHai
  * @date 2021/2/7 18:56
  */
-//刚刚配置了gitee与GitHub，看看提交时是不是一下提交两个托管平台
+//刚刚配置了gitee与GitHub，看看提交时是不是一下提交两个托管平台////答案是只提交到了GitHub上
     @Configuration  // 指定当前类是一个配置类 写这个注解，但是在获取容器时可以传入配置类的字节码文件，所以这里用不用这个注解都无所谓
     @ComponentScan("in2021winter.annotation") //扫描要创建容器的包，相当于xml中的<context:component-scan base-package="in2021winter.huanghai"></context:component-scan>
     @Import({JdbcConfig.class,TransactionManager.class})// 导入子配置类，有该注解的被视为父配置类，被导入的为子配置类
